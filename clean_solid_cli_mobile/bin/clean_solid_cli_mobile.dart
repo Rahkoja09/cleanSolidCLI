16d52e7a8a3dbd520740bffb4349a94f597b075a
Rahkoja09 <ramanamahefakoja.mg@gmail.com>
2026-03-01 14:52:07 +0300

    initial commit

---
+++ clean_solid_cli_mobile/bin/clean_solid_cli_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:clean_solid_cli_mobile/clean_solid_cli_mobile.dart' as clean_solid_cli_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clean_solid_cli_mobile.calculate()}!');
+}
---
+++ clean_solid_cli_mobile/test/clean_solid_cli_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:clean_solid_cli_mobile/clean_solid_cli_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
